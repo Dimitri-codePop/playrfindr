5fdf5fbb6652318e35ae7e9dccd780e167ae58f0
Antoinenoix <noix.antoine@gmail.com>
2021-06-09 10:44:27 +0200

    creation data fictive

---
+++ front/src/data/event.js
@@ -0,0 +1,21 @@
+export default {
+  "total_events": 2,
+  "event": [
+    {
+      "id": 1,
+      "content": "Petite soirée jeu à mon appart",
+      "date": "2021-06-09 19:00:00",
+      "lieu": 75,
+      "nb_maxplayer": 5,
+      "users":[1, 3],
+    },
+    {
+      "id": 1,
+      "content": "Aprem jeu au parc de la villette",
+      "date": "2021-06-12 15:00:00",
+      "lieu": 31,
+      "nb_maxplayer": 4,
+      "users":[4, 5],
+    },
+  ]
+}
