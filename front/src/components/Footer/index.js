bb8ee9a8ed096d520cd02ce2dd5591577cb2e780
Paulhennin <henninpaul@gmail.com>
2021-06-10 10:08:04 +0200

    footer done with Links

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
   faCopyright,
@@ -17,21 +17,35 @@ export default function Footer() {
     <footer>
       <div className="footer__grid">
         <h2 className="footer__title">Support</h2>
+        <Link exact to="/contact">
         <span>Contactez-nous</span>
+        </Link>
+        <Link exact to="/faq">
         <span>FAQ</span>
+        </Link>
       </div>
       <div className="footer__grid">
         <h2 className="footer__title">A propos</h2>
+        <Link exact to="/cgu">
         <span>CGU</span>
+        </Link>
+        <Link exact to="/legals">
         <span>Mentions légales</span>
+        </Link>
         <span><FontAwesomeIcon icon={faCopyright} /> PlayrFindr</span>
       </div>
       <div className="footer__grid"> 
         <h2 className="footer__title">Retrouvez nous</h2>
         <div>
+        <a href="http://www.facebook.com">
         <FontAwesomeIcon icon={faFacebookSquare} className="footer__brands-icons"/>
+          </a>
+          <a href="http://www.instagram.com">
         <FontAwesomeIcon icon={faInstagramSquare} className="footer__brands-icons"/>
+          </a>
+          <a href="http://www.twitter.com">
         <FontAwesomeIcon icon={faTwitterSquare} className="footer__brands-icons"/>
+          </a>
         </div>
       </div>
     </footer>
