04f0239f4fcf2910833183b890ffcf5125824f92
Paulhennin <henninpaul@gmail.com>
2021-06-10 10:26:03 +0200

    :construction: setup homPage

---
+++ front/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+export default function Home() {
+  return (
+<div>App</div>
+);
+}
+
+Home.propTypes = {
+
+};
