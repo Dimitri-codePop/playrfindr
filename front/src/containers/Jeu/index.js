92ebf109ff5d4a5560d8bfe5d05f4985525aceb3
Antoinenoix <noix.antoine@gmail.com>
2021-06-16 10:23:45 +0200

    header mw onegame update

--- front/src/containers/Jeu/index.js
+++ front/src/containers/Jeu/index.js
@@ -11,7 +11,7 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  loadGame: () => {
+  loadGame: (id) => {
     dispatch(fetchGame(id));
   },
 });
