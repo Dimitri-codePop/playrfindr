74ab456f8813b3c939c1e62a841b3a263cd842a4
Dimitri-codePop <dimmartin32@gmail.com>
2021-06-16 12:42:13 +0200

    Fix des events

--- app/controllers/messageController.js
+++ app/controllers/messageController.js
@@ -1,7 +1,12 @@
-/* const MessageModel = require('../models/messageModel');
+const MessageModel = require('../models/messageModel');
 
 module.exports = {
-    async getAll(_, res){
-        const user = await MessageModel.findAllMessage() 
+    async getAll(req, res, next){
+        
+        const user = await MessageModel.findAllMessageByUser(req.user.userId);
+          if(!user){
+              return next();
+          }
+          return res.status(200).json({data: user})
     }
-} */
+} 
