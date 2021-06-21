1c279b41b10c161bbbe6d94f51de017ff97564cf
Antoinenoix <noix.antoine@gmail.com>
2021-06-21 13:09:21 +0200

    css modal sign in, sign up, correction events

--- front/src/components/Home/Modals/Signup/Form/Field/index.js
+++ front/src/components/Home/Modals/Signup/Form/Field/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+
 
 import './styles.scss';
 
 export default function Field({ type, name, placeholder, value, onChange }) {
+
   const handleOnChange = (event) => {
     onChange(event.target.value, name);
   };
 
   return (
     <div className="field">
-      <input
+      <TextField 
         className="field__input"
+        required id="standard-required" 
+        label={placeholder}
         type={type}
         name={name}
         id={name}
-        placeholder={placeholder}
         value={value}
         onChange={handleOnChange}
       />
@@ -34,3 +38,4 @@ Field.propTypes = {
 Field.defaultProps = {
   type: 'text',
 };
+
